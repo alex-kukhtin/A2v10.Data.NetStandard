6802712d356bb3d8c4a4c5d11b81a2a257a499ae
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-02-25 09:51:21 +0200

    Add project files.

---
+++ A2v10.Data.Interfaces/ITenantManager.cs
@@ -0,0 +1,14 @@
+﻿// Copyright © 2015-2020 Alex Kukhtin. All rights reserved.
+
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace A2v10.Data.Interfaces
+{
+	public interface ITenantManager
+	{
+		Task SetTenantIdAsync(SqlConnection cnn, String source);
+		void SetTenantId(SqlConnection cnn, String source);
+	}
+}
